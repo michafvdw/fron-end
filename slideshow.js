1b73df6aff558f98a307dd8b79abf2bbb7b13f9b
Micha <74900539+michafvdw@users.noreply.github.com>
2020-12-23 00:16:45 +0100

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,15 @@
+// Automatic Slideshow change image every 5 seconds
+var myIndex = 0;
+slideshow();
+
+function slideshow() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(slideshow, 5000);
+}
---
+++ submitOnEvent.js
@@ -0,0 +1,5 @@
+function submitEvent() {
+    var txt;
+    if (confirm("je hebt een reservering geplaatst"))
+    document.getElementById("demo").innerHTML = txt;
+}
